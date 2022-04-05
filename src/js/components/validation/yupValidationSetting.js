ef864579099996f997bc32f5733ab0fa1e447fd1
Tomáš Janík <tom.jajom@gmail.com>
2022-04-05 19:22:16 +0200

    yup validation Translation

--- src/js/components/validation/yupValidationSetting.js
+++ src/js/components/validation/yupValidationSetting.js
@@ -2,45 +2,45 @@
 import * as Yup from 'yup';
 
 export const personInfo = Yup.object({
-	sureName: Yup.string().required('Vyplňte prosím jméno.'),
-	familyName: Yup.string().required('Vyplňte prosím příjmení.'),
-	birthPlace: Yup.string().required('Vyplňte prosím místo narození.'),
+	sureName: Yup.string().required('Будь ласка, введіть ім я.'),
+	familyName: Yup.string().required('Заповніть, будь ласка, прізвище.'),
+	birthPlace: Yup.string().required('Заповніть, будь ласка, місце народження.'),
 	idNumber: Yup.string()
-		.required('Vyplňte prosím číslo občanského průkazu.')
-		.matches(/^([a-z]{2})?\d{4,}$/i, 'Vyplňte prosím číslo občanského průkazu.'),
+		.required('Будь ласка, заповніть номер візи.')
+		.matches(/^([a-z]{2})?\d{4,}$/i, 'Будь ласка, заповніть номер візи.'),
 	birthDate: Yup.string()
-		.required('Vyplňte prosím datum narození.')
-		.matches(/^[0-3][0-9]\.[0-1][0-9]\.[1-2][0-9]{3}$/, 'Chybný formát data narození (DD.MM.RRRR).')
-		.test('min-age-15', 'Musíte být starší 15 let', (value) => {
+		.required('Будь ласка, заповніть дату свого народження.')
+		.matches(/^[0-3][0-9]\.[0-1][0-9]\.[1-2][0-9]{3}$/, 'Неправильний формат дати (DD.MM.YYYY).')
+		.test('min-age-15', 'Вам має бути більше 15 років', (value) => {
 			const [day, month, year] = value.split('.');
 			const age = new Date(Date.now() - new Date(year, month - 1, day)).getUTCFullYear() - 1970;
 			return age >= 15;
 		}),
 	birthNumber: Yup.string()
-		.required('Vyplňte prosím rodné číslo.')
-		.matches(/^[0-9]{6}[0-9A-Za-z]{3,4}$/, 'Chybný formát rodného čísla.'),
+		.required('Будь ласка, заповніть свій номер народження.')
+		.matches(/^[0-9]{6}[0-9A-Za-z]{3,4}$/, 'Неправильний формат номера народження.'),
 	gender: Yup.string()
-		.required('Zvolte prosím vaše pohlaví.')
-		.oneOf(['1', '2'], 'Zvolte prosím vaše pohlaví.'),
+		.required('Будь ласка, виберіть свою стать.')
+		.oneOf(['1', '2'], 'ZБудь ласка, виберіть свою стать.'),
 });
 
 const contactAddress = Yup.object({
-	street: Yup.string().required('Vyplňte prosím ulici a č.p.'),
-	city: Yup.string().required('Vyplňte prosím město.'),
+	street: Yup.string().required('Будь ласка, заповніть вулицю та описовий номер.'),
+	city: Yup.string().required('Будь ласка, заповніть місто.'),
 	zip: Yup.string()
-		.required('Vyplňte prosím PSČ')
-		.matches(/^[0-9]{3}[ ]?[0-9]{2}$/, 'Chybný formát PSČ.'),
-	country: Yup.string().required('Zvolte prosím stát.'),
+		.required('Будь ласка, заповніть поштовий індекс')
+		.matches(/^[0-9]{3}[ ]?[0-9]{2}$/, 'Неправильний формат поштового індексу.'),
+	country: Yup.string().required('Виберіть країну.'),
 });
 
 export const contactsInfo = Yup.object({
 	permanentAddress: Yup.object({
-		street: Yup.string().required('Vyplňte prosím ulici a č.p.'),
-		city: Yup.string().required('Vyplňte prosím město.'),
+		street: Yup.string().required('Будь ласка, заповніть вулицю та описовий номер.'),
+		city: Yup.string().required('Будь ласка, заповніть місто.'),
 		zip: Yup.string()
-			.required('Vyplňte prosím PSČ')
-			.matches(/^[0-9]{3}[ ]?[0-9]{2}$/, 'Chybný formát PSČ.'),
-		country: Yup.string().required('Zvolte prosím stát.'),
+			.required('Будь ласка, заповніть поштовий індекс')
+			.matches(/^[0-9]{3}[ ]?[0-9]{2}$/, 'Неправильний формат поштового індексу.'),
+		country: Yup.string().required('Виберіть країну.'),
 	}),
 
 	contactAddress: Yup.object()
@@ -53,33 +53,33 @@ export const contactsInfo = Yup.object({
 			zip: Yup.string(),
 			country: Yup.string(),
 		})
-		.test('has-contact-address', 'Zadejte kontaktní adresu v ČR', function(value) {
+		.test('has-contact-address', 'Введіть контактну адресу в Чеській Республіці', function(value) {
 			return !(this.parent.permanentAddress.country === 'sk' && value.country !== 'cz');
 		}),
 
 	phone: Yup.string()
-		.required('Vyplňte prosím telefonní číslo.')
+		.required('Будь ласка, заповніть номер телефону.')
 		.matches(
 			/^\+(420|421)[ ]{0,1}[0-9]{3}[ ]{0,1}[0-9]{3}[ ]{0,1}[0-9]{3}$/,
-			'Zadejte telefonní číslo v požadovaném formátu: +420123456789',
+			'Введіть номер телефону в потрібному форматі: +420123456789',
 		),
 });
 
 export const loginInfo = Yup.object({
 	email: Yup.string()
-		.required('Vyplňte prosím e-mail.')
-		.email('E-mail není ve správném formátu.'),
+		.required('Будь ласка, заповніть e-mail.')
+		.email('Електронна пошта не в правильному форматі.'),
 	password: Yup.string()
-		.required('Vyplňte prosím heslo.')
-		.matches(/^[a-zA-Z0-9]+$/, 'Heslo může obsahovat pouze malá/velká písmena a čísla.')
-		.min(6, 'Heslo musí mít alespoň 6 znaků.'),
+		.required('Будь ласка, введіть пароль.')
+		.matches(/^[a-zA-Z0-9]+$/, 'Пароль може містити лише малі / великі літери та цифри.')
+		.min(6, 'Пароль має містити не менше 6 символів.'),
 	passwordCheck: Yup.string()
-		.required('Vyplňte prosím heslo pro ověření.')
-		.oneOf([Yup.ref('password')], 'Hesla se musí shodovat.'),
+		.required('Введіть пароль для підтвердження.')
+		.oneOf([Yup.ref('password')], 'Паролі повинні збігатися.'),
 	acceptDataProcessing: Yup.boolean()
-		.oneOf([true], 'Potvrďte prosím souhlas s pravidly.')
+		.oneOf([true], 'Будь ласка, підтвердьте свою згоду з правилами.')
 		.default(false),
 	reCaptcha: Yup.string()
 		.nullable()
-		.required('Potvrďte prosím, že nejste robot.'),
+		.required('Будь ласка, підтвердьте, що ви не робот.'),
 });
